d16700b1ef5dbbdbbd54006429228a8c2abd15af
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-04-07 19:09:56 +0400

    [CVFV-20] fixed regex.

--- src/ESVatFormatValidator.php
+++ src/ESVatFormatValidator.php
@@ -6,7 +6,7 @@ use rocketfellows\CountryVatFormatValidatorInterface\CountryVatFormatValidator;
 
 class ESVatFormatValidator extends CountryVatFormatValidator
 {
-    private const VAT_NUMBER_PATTERN = '/(^(ES)?([0-9A-Z]\d{7}[0-9A-Z]$))/';
+    private const VAT_NUMBER_PATTERN = '/^(ES)?([A-Z]{1}\d{8}$|\d{8}[A-Z]{1}$|[A-Z]{1}\d{7}[A-Z]{1}$)/';
 
     protected function isValidFormat(string $vatNumber): bool
     {
